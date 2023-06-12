3b66a8863f4f8f1325075025aaf036697f87ccce
unknown <bharathsrini19@gmail.com>
2023-06-12 13:31:27 -0400

    cors chg

--- app.js
+++ app.js
@@ -16,13 +16,35 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(
-  cors({
-    credentials: true,
-    origin: "https://bharathraamwebdeva5.netlify.app",
-  })
-);
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: "https://bharathraamwebdeva5.netlify.app",
+//   })
+// );
 // const port = process.env.PORT || 4000;
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://bharathraamwebdeva5.netlify.app",
+  ];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, POST, DELETE, PATCH, OPTIONS"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 TuitsController(app);
 HelloController(app);
